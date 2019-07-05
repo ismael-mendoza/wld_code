7a98c2334d794dc514fcf8a1439b0b502fe83a7e
Ismael Salvador Mendoza Serrano <imendoza@stanford.edu>
2019-07-05 08:44:07 -0700

    reflection finished, should discuss with Pat

--- mycode/weights.py
+++ mycode/weights.py
@@ -1,5 +1,9 @@
 import numpy as np
 
+
+def cut_both(dbt, cut1, cut2): 
+    return dbt[(dbt>=cut1)&(dbt<=cut2)]
+
 #param is usually one of the biases. 
 def get_iso_or_grp_suffix(iso_or_grp):
     suffix = ''
@@ -7,14 +11,86 @@ def get_iso_or_grp_suffix(iso_or_grp):
         suffix = '_grp'
         
     return suffix 
+
+
+def boots_fnc_simple(dbt, fnc, N=1000):
+    results = [] 
+    for i in range(N): 
+        results.append(fnc(np.random.choice(dbt,len(dbt), replace=True)))
+    return results 
+
+def boots_fnc(orig_ids, cats, fnc, N=1000, args=[]):
+    bootstrap_ids = [np.random.choice(orig_ids,len(orig_ids), replace=True) for _ in range(N)] 
+    results = [[fnc(ids, cat, *args) for ids in bootstrap_ids] for cat in cats]  
+    return results 
+
+
+####### weighted means. 
     
 
-def get_weights(args, component, iso_or_grp): 
+def get_weights(ids, cat, component, iso_or_grp, which_shape_noise='component'): 
     suffix = get_iso_or_grp_suffix(iso_or_grp)
-    shape_noise = np.std(args[f'e{component}'])**2 
-    weights = (shape_noise + args[f'dg{component}{suffix}']**2)**(-1)
+    
+    if which_shape_noise=='component': 
+        shape_noise = np.std(cat[f'e{component}'][ids])**2
+    elif which_shape_noise=='magnitude': 
+        shape_noise = np.std(np.sqrt(cat['e1'][ids]**2 + cat['e2'][ids]**2))**2
+        
+        
+    weights = (shape_noise + cat[f'dg{component}{suffix}'][ids]**2)**(-1)
     
     return weights 
+
+#which_shape_noise in []'component','magnitude'
+def wmean(ids, cat, component, iso_or_grp, which_shape_noise):
+    assert component in ['1','2'], 'component value is not valid. '
+    assert iso_or_grp in ['iso', 'grp'], 'iso_or_grp received invalied argument'
+    assert which_shape_noise in ['component', 'magnitude'], 'invalid agument for which_shape_noise'
+    
+    suffix = get_iso_or_grp_suffix(iso_or_grp)
+    weights = get_weights(ids, cat, component, iso_or_grp, which_shape_noise)
+    param = f'bias_g{component}{suffix}'
+    dbt = cat[param][ids]
+    return np.sum(weights*dbt)/np.sum(weights)
+
+
+
+#all catalogues must be same size (so ids make sense) and sorted. 
+def get_errors(orig_ids, cats, fnc, N=1000, args=[]): 
+    results = boots_fnc(orig_ids, cats, fnc, N, args)
+    return [np.std(cat_results) for cat_results in results]
+
+
+
+#### clippings means 
+
+
+def clipped_mean(ids, cat, param, p): 
+    dbt = cat[param][ids]
+    return clipped_mean_simple(dbt, p)
+
+def clipped_mean_simple(dbt, p):
+    
+    if p == 0.5: 
+        return np.median(dbt)
+
+    q1 = np.quantile(dbt, p)
+    q2 = np.quantile(dbt, 1-p)
+    cut_dbt = cut_both(dbt, q1, q2)
+    mean = np.mean(cut_dbt)
+    return mean 
+
+def clipped_mean_fnc(p): 
+    return lambda dbt: clipped_mean_simple(dbt, p)
+
+
+def get_error(dbt, fnc, N=1000): 
+    results = boots_fnc_simple(dbt, fnc, N)
+    return np.std(results)
+
+
+### misc. 
+
 
 def get_not_dropped_cat(cat): 
     grps = np.sort(list(set(cat['grp_id']))) #ids of all the groups, sorted. 
@@ -39,42 +115,3 @@ def get_not_dropped_cat(cat):
         
     grps_not_dropped = np.array(grps)[np.array(grp_dropped) == 0]
     return get_slice(cat, 'grp_id', set(list(grps_not_dropped)))
-
-
-def wmean_func(dbt, cat, component, iso_or_grp):
-    assert component in ['1','2'], 'component value is not valid. '
-    assert iso_or_grp in ['iso', 'grp'], 'iso_or_grp received invalied argument'
-    
-    suffix = get_iso_or_grp_suffix(iso_or_grp)
-    weights = get_weights(cat, component, iso_or_grp)
-    return np.sum(weights*dbt)/np.sum(weights) 
-
-def wmean(args, component, iso_or_grp): 
-    return lambda dbt: wmean_func(dbt, args, component, iso_or_grp)
-
-
-def get_error(dbt, fnc, N=1000): 
-    results = boot_fnc(dbt, fnc, N)
-    return np.std(results)
-
-def boot_fnc(dbt, fnc, N=1000):
-    results = [] 
-    for i in range(N): 
-        results.append(fnc(np.random.choice(dbt,len(dbt), replace=True)))
-    return results 
-
-def clipped_mean(dbt, p): 
-    if p == 0.5: 
-        return np.median(dbt)
-    
-    q1 = np.quantile(dbt, p)
-    q2 = np.quantile(dbt, 1-p)
-    cut_dbt = cut_both(dbt, q1, q2)
-    mean = np.mean(cut_dbt)
-    return mean 
-
-def clipped_mean_fnc(p): 
-    return lambda dbt: clipped_mean(dbt, p) 
-
-def cut_both(dbt, cut1, cut2): 
-    return dbt[(dbt>=cut1)&(dbt<=cut2)]
